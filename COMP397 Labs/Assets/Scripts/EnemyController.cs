17e1f95a0738732403c985fbf0c367e533ba3f33
Dhevan Lau <dhevan999@gmail.com>
2021-06-11 16:34:25 +0200

    Enemy Nav Mesh

---
+++ COMP397 Labs/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+
+    public Transform playerT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(playerT.position);
+    }
+}
